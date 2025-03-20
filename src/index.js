e669aea5bdc5c81f34ac18491abe2ab051c0fde2
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-03-20 21:15:50 +0300

    fetch2

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ const init = () => {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+    const inputForm = document.querySelector("form");
+  
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init)
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+  });
